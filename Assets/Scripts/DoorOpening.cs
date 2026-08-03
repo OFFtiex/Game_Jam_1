333d3960535bd3bec572a46c798a4dfb926364e4
OFFtiex <gera.maslov.39@gmail.com>
2026-08-03 16:18:55 +0200

    add door script

--- Assets/Scripts/DoorOpening.cs
+++ Assets/Scripts/DoorOpening.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 
 public class DoorOpening : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+{   
+    public SpriteRenderer Door_model;
+    public Sprite new_sprite; 
+
+    private BoxCollider2D boxCollider;
+
+    void Start(){
+    boxCollider = GetComponent<BoxCollider2D>();
+    Player player = GetComponent<Player>();
+    Door_model = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnCollisionEnter2D(Collision2D collider){
+        if (collider.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.Space)){
+            Door_model.sprite = new_sprite;
+            boxCollider.size = new Vector3(0,0,-1);
+        }
     }
 }
