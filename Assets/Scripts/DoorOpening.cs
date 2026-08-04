4954244e33a30793d5e5961d4efc23f0d572dd03
OFFtiex <gera.maslov.39@gmail.com>
2026-08-04 12:54:39 +0200

    added door script

--- Assets/Scripts/DoorOpening.cs
+++ Assets/Scripts/DoorOpening.cs
@@ -1,22 +1,47 @@
+// using UnityEngine;
+
+// public class DoorOpening : MonoBehaviour
+// {   
+//     public SpriteRenderer Door_model;
+//     public Sprite new_sprite; 
+
+//     private BoxCollider2D boxCollider;
+
+//     void Start(){
+//     boxCollider = GetComponent<BoxCollider2D>();
+
+//     Door_model = GetComponent<SpriteRenderer>();
+//     }
+
+//     private void OnCollisionEnter2D(Collision2D collider){
+//         HasKey isKey = collider.gameObject.GetComponent<HasKey>();
+//         if (isKey == null) return;
+//         if (collider.gameObject.CompareTag("Player") && isKey.hasKey){
+//             switch (isKey.color)
+//             {   
+//                 case "Red":
+//                     Door_model.sprite = new_sprite;
+//                     boxCollider.size = new Vector2(0,0);
+//                     break;
+                
+//                 default: return;
+//             }
+//         }
+//     }
+// }
 using UnityEngine;
 
 public class DoorOpening : MonoBehaviour
 {   
     public SpriteRenderer Door_model;
     public Sprite new_sprite; 
-
-    private BoxCollider2D boxCollider;
+    public BoxCollider2D boxCollider;
 
     void Start(){
     boxCollider = GetComponent<BoxCollider2D>();
-    Player player = GetComponent<Player>();
     Door_model = GetComponent<SpriteRenderer>();
     }
 
-    private void OnCollisionEnter2D(Collision2D collider){
-        if (collider.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.Space)){
-            Door_model.sprite = new_sprite;
-            boxCollider.size = new Vector3(0,0,-1);
-        }
-    }
+    // Door_model.sprite = new_sprite;
+    // boxCollider.size = new Vector2(0,0);
 }
