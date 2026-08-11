43d5e4b649bcabb23b1639da84b731bf5463d0d2
Gunlin <r.a.ft@mail.ru>
2026-08-11 14:49:43 +0200

    Порталы работают корректно, но только при первом запуске сцены

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -1,17 +1,22 @@
-using UnityEngine.SceneManagement;
-using UnityEngine;
 using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class Teleport : MonoBehaviour
 {
+    private static float nextTeleportTime;
+
     [SerializeReference]
     private ITeleportLogic logic;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player") && logic != null)
+        if (other.CompareTag("Player") && logic != null && Time.time >= nextTeleportTime)
         {
+            nextTeleportTime = Time.time + 0.5f;
+
             logic.Execute(other.gameObject);
         }
     }
@@ -45,3 +50,62 @@ public class NextLevelTeleport : ITeleportLogic
         }
     }
 }
+
+
+// ==========================================
+// CUSTOM INSPECTOR (Will only work in the editor)
+// ==========================================
+
+#if UNITY_EDITOR
+
+[CustomEditor(typeof(Teleport))]
+public class TeleportEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        SerializedProperty logicProperty = serializedObject.FindProperty("logic");
+
+        int currentIndex = logicProperty.managedReferenceValue switch
+        {
+            NextLevelTeleport => 2,
+            LocalTeleport => 1,
+            _ => 0
+        };
+
+        string[] options = { "Not selected", "Doubles", "Next level" };
+
+        EditorGUI.BeginChangeCheck();
+        int newIndex = EditorGUILayout.Popup("Teleport type", currentIndex, options);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(target, "Change Teleport Type");
+
+            if (newIndex == 0)
+            {
+                logicProperty.managedReferenceValue = null;
+            }
+            else
+            {
+                System.Type targetType = newIndex == 2 ? typeof(NextLevelTeleport) : typeof(LocalTeleport);
+
+                object newInstance = System.Activator.CreateInstance(targetType);
+                logicProperty.managedReferenceValue = newInstance;
+            }
+
+            EditorUtility.SetDirty(target);
+        }
+
+        if (newIndex > 0 && logicProperty.managedReferenceValue != null)
+        {
+            EditorGUILayout.Space(5);
+            EditorGUILayout.LabelField("Teleport Settings", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(logicProperty, GUIContent.none, true);
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+#endif
